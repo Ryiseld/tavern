35e8aa10924ebad3ded8b2f33d846e02a634886e
Ryiseld <ryiseld@gmail.com>
2016-10-09 18:48:18 +0300

    Updated TinyMCE plugins and style

--- public/js/tinymce.js
+++ public/js/tinymce.js
@@ -1,8 +1,13 @@
 tinymce.init({
     selector: 'textarea.tinymce',
+    content_css: '/css/tinymce.css',
     height: 250,
+    width: '99%',
     statusbar: false,
     menubar: false,
     plugins: 'textcolor link image emoticons',
-    toolbar: 'undo redo bold italic underline alignleft aligncenter alignright bullist numlist forecolor backcolor link image emoticons',
+    toolbar: [
+    	'undo redo bold italic underline fontsizeselect alignleft aligncenter alignright',
+    	'bullist numlist forecolor backcolor link image emoticons'
+    ]
 });
